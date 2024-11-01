645af341f5da575e6a6d5409b4833fd5464076dd
Administrator <Administrator@DESKTOP-AV2PARO>
2024-11-01 08:27:54 +0530

    Second commit

---
+++ GroceryApplication/src/test/java/constant/Constant.java
@@ -0,0 +1,18 @@
+package constant;
+
+public class Constant {
+	
+	public static final String lp_verifyLoginWithValidCredentials=": Heading Text not As Expected";
+	public static final String SubCategorynotcreatedalrtstatus=": Sub category created alert message not as expected";
+	public static final String SubCategorynameStatus=": Sub Category Name Not As Expected";
+	public static final String deleteAlertStatus=": delete alert not as expected";
+	public static final String deletedItemStatus=": deleted item present in table";
+	public static final String validSearchStatus=": valid search result not as expected";
+	public static final String invalidSearchStatus=": invalid search result not as expected";
+	public static final String invalidUpdateContactStatus=": contact update alert not as expected";
+	public static final String newsaddisionStatus=": news addison alert not as expected";
+	public static final String tablenotcontainsupdateditem=": item not updated as expected";
+	public static final String serchnotcontainsearcheditem=": search item not found";
+	public static final String Newsaddedstatus=": added news not visibile in news table";
+
+}
